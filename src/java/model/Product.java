0912660cf5c997ae838d07463521d18d5d656b6e
Babatope Shokunbi <babathurpe@yahoo.com>
2015-02-21 09:47:15 -0500

    Added product model class

---
+++ src/java/model/Product.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Babathurpe
+ */
+public class Product {
+    private int productId;
+    private String name;
+    private String description;
+    private int quantity;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "productId=" + productId + ", name=" + name + ", description=" + description + ", quantity=" + quantity + '}';
+    }
+    
+    
+}
