27899b6c1c06480cb7e090d97d6846d2f468cf9c
Babatope Shokunbi <babathurpe@yahoo.com>
2015-02-23 21:53:53 -0500

    Made code prettier and better output

--- src/java/servlets/ProductServlet.java
+++ src/java/servlets/ProductServlet.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Babathurpe
  */
-@WebServlet("/product")
+@WebServlet("/products")
 public class ProductServlet extends HttpServlet {
 
     private String getResults(String query, String... params) {
@@ -38,9 +38,9 @@ public class ProductServlet extends HttpServlet {
             sb.append("[");
             while (rs.next()) {
                 //sb.append(String.format("%s\t%s\t%s\n", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
-                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %d },", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
+                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %d },\n", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
             }
-            sb.substring(0, sb.length() - 1);
+            sb.setLength(sb.length()-2);
             sb.append("]");
         } catch (SQLException ex) {
             Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
@@ -48,22 +48,22 @@ public class ProductServlet extends HttpServlet {
         return sb.toString();
     }
 
-//    private String getSingleResult(String query, String... params) {
-//        StringBuilder sb = new StringBuilder();
-//        try (Connection conn = DbConnection.getConnection()) {
-//            PreparedStatement pstmt = conn.prepareStatement(query);
-//            for (int i = 1; i <= params.length; i++) {
-//                pstmt.setString(i, params[i - 1]);
-//            }
-//            ResultSet rs = pstmt.executeQuery();
-//            while (rs.next()) {
-//                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %s }", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
-//            }
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return sb.toString();
-//    }
+    private String getSingleResult(String query, String... params) {
+        StringBuilder sb = new StringBuilder();
+        try (Connection conn = DbConnection.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %s }", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sb.toString();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -75,7 +75,7 @@ public class ProductServlet extends HttpServlet {
             } else {
                 // There are some parameters
                 int id = Integer.parseInt(request.getParameter("id"));
-                out.println(getResults("SELECT * FROM product WHERE productid = ?", String.valueOf(id)));
+                out.println(getSingleResult("SELECT * FROM product WHERE productid = ?", String.valueOf(id)));
             }
         } catch (IOException ex) {
             Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
