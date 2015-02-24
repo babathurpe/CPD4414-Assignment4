4d80e1252861f6eaf211d42e4276fe4f2e933873
Babatope Shokunbi <babathurpe@yahoo.com>
2015-02-23 20:56:30 -0500

    Added doPut Method and update method

--- src/java/servlets/ProductServlet.java
+++ src/java/servlets/ProductServlet.java
@@ -48,22 +48,22 @@ public class ProductServlet extends HttpServlet {
         return sb.toString();
     }
 
-    private String getSingleResult(String query, String... params) {
-        StringBuilder sb = new StringBuilder();
-        try (Connection conn = DbConnection.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            for (int i = 1; i <= params.length; i++) {
-                pstmt.setString(i, params[i - 1]);
-            }
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %s }", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return sb.toString();
-    }
+//    private String getSingleResult(String query, String... params) {
+//        StringBuilder sb = new StringBuilder();
+//        try (Connection conn = DbConnection.getConnection()) {
+//            PreparedStatement pstmt = conn.prepareStatement(query);
+//            for (int i = 1; i <= params.length; i++) {
+//                pstmt.setString(i, params[i - 1]);
+//            }
+//            ResultSet rs = pstmt.executeQuery();
+//            while (rs.next()) {
+//                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %s }", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return sb.toString();
+//    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -75,7 +75,7 @@ public class ProductServlet extends HttpServlet {
             } else {
                 // There are some parameters
                 int id = Integer.parseInt(request.getParameter("id"));
-                out.println(getSingleResult("SELECT * FROM product WHERE productid = ?", String.valueOf(id)));
+                out.println(getResults("SELECT * FROM product WHERE productid = ?", String.valueOf(id)));
             }
         } catch (IOException ex) {
             Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
@@ -137,11 +137,45 @@ public class ProductServlet extends HttpServlet {
     }
     
 
-    private int delete(String query, int qty) {
+    private int delete(String query, int id) {
         int numChanges = 0;
         try (Connection conn = DbConnection.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setInt(1, qty);
+            pstmt.setInt(1, id);
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numChanges;
+    }
+    
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Set<String> keySet = request.getParameterMap().keySet();
+        if (keySet.contains("name") && keySet.contains("description") && keySet.contains("quantity")) {
+                // There are some parameters                
+                String name = request.getParameter("name");
+                String description = request.getParameter("description");
+                String quantity = request.getParameter("quantity");
+                int qty = Integer.parseInt(quantity);
+            int id = Integer.parseInt(request.getParameter("id"));
+            update("UPDATE product SET name = ?, description = ?, quantity = ? WHERE productid = ?", name, description, qty, id);
+        } else {
+            response.setStatus(500);
+        }
+    }
+    
+    private int update(String query,String name, String desc, int qty,int id) {
+        int numChanges = 0;
+        ArrayList params = new ArrayList();
+        params.add(name);
+        params.add(desc);
+        params.add(qty);
+        try (Connection conn = DbConnection.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.size(); i++) {
+                pstmt.setString(i, params.get(i - 1).toString());
+            }
             numChanges = pstmt.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
