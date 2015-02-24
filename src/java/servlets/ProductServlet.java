5885d22e3f0986d1e212b8290d6e94f46cc2f5f6
Babatope Shokunbi <babathurpe@yahoo.com>
2015-02-23 19:55:05 -0500

    Added doGet Method and 2 getResult methods for each type of GET request

--- src/java/servlets/ProductServlet.java
+++ src/java/servlets/ProductServlet.java
@@ -5,10 +5,82 @@
  */
 package servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author Babathurpe
  */
-public class ProductServlet {
+@WebServlet("/product")
+public class ProductServlet extends HttpServlet {
+
+    private String getResults(String query, String... params) {
+        StringBuilder sb = new StringBuilder();
+        try (Connection conn = DbConnection.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            sb.append("[");
+            while (rs.next()) {
+                //sb.append(String.format("%s\t%s\t%s\n", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
+                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %d },", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
+            }
+            sb.substring(0, sb.length() - 1);
+            sb.append("]");
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sb.toString();
+    }
+
+    private String getSingleResult(String query, String... params) {
+        StringBuilder sb = new StringBuilder();
+        try (Connection conn = DbConnection.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %s }", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Content-Type", "text/plain-text");
+        try (PrintWriter out = response.getWriter()) {
+            if (!request.getParameterNames().hasMoreElements()) {
+                // There are no parameters at all
+                out.println(getResults("SELECT * FROM product"));
+            } else {
+                // There are some parameters
+                int id = Integer.parseInt(request.getParameter("id"));
+                out.println(getSingleResult("SELECT * FROM product WHERE productid = ?", String.valueOf(id)));
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     
 }
