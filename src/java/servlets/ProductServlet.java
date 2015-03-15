f3286e474e067b1f91ca8a6614f7552fc62c6fd4
Babatope Shokunbi <babathurpe@yahoo.com>
2015-03-14 22:48:47 -0400

    Updated to assignment 5

--- src/java/servlets/ProductServlet.java
+++ src/java/servlets/ProductServlet.java
@@ -13,6 +13,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -32,99 +36,58 @@ import org.json.simple.parser.ParseException;
 //@WebServlet("/products")
 @Path("/products")
 public class ProductServlet {
-
-    private String getResults(String query, String... params) {
-        StringBuilder sb = new StringBuilder();
-        try (Connection conn = DbConnection.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            for (int i = 1; i <= params.length; i++) {
-                pstmt.setString(i, params[i - 1]);
-            }
-            ResultSet rs = pstmt.executeQuery();
-            sb.append("[");
-            while (rs.next()) {
-                //sb.append(String.format("%s\t%s\t%s\n", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
-                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %d },\n", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
-            }
-            sb.setLength(sb.length() - 2);
-            sb.append("]");
-        } catch (SQLException ex) {
-            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return sb.toString();
-    }
-
-    private String getSingleResult(String query, String... params) {
-        StringBuilder sb = new StringBuilder();
-        try (Connection conn = DbConnection.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            for (int i = 1; i <= params.length; i++) {
-                pstmt.setString(i, params[i - 1]);
-            }
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                sb.append(String.format("{ \"ProductId\" : %d, \"name\": \"%s\", \"description\": \"%s\", \"quantity\": %s }", rs.getInt("productid"), rs.getString("name"), rs.getString("description"), rs.getInt("quantity")));
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(ProductServlet.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return sb.toString();
-    }
-
+    
+    private JsonObject jsonData;
+    
     @GET
     @Produces("application/json; charset=UTF-8")
     public String doGet() throws SQLException {
-        JSONArray jsonArray = new JSONArray();
+        //JSONArray jsonArray = new JSONArray();
+        JsonArrayBuilder productsList = Json.createArrayBuilder();
         Connection conn = DbConnection.getConnection();
         String query = "SELECT * FROM product";
         PreparedStatement pstmt = conn.prepareStatement(query);
 
         ResultSet rs = pstmt.executeQuery();
-        while (rs.next()) {
-            int total_columns = rs.getMetaData().getColumnCount();
-            JSONObject jsonData = new JSONObject();
-            for (int i = 0; i < total_columns; i++) {
-                String columnName = rs.getMetaData().getColumnLabel(i + 1).toLowerCase();
-                Object columnValue = rs.getObject(i + 1);
-                jsonData.put(columnName, columnValue);
-            }
-            jsonArray.add(jsonData);
-
-        }
-        return jsonArray.toJSONString();
+            
+            while (rs.next()) {
+                    JsonObjectBuilder productBuilder = Json.createObjectBuilder();
+                    productBuilder.add("productId", rs.getInt("productid"))
+                            .add("name", rs.getString("name"))
+                            .add("description", rs.getString("description"))
+                            .add("quantity", rs.getInt("quantity"));
+                    productsList.add(productBuilder);
+                }
+            return productsList.build().toString();
     }
 
     @GET
     @Produces("application/json; charset=UTF-8")
     @Path("{productid}")
     public String doGet(@PathParam("productid") int id) throws SQLException {
-        JSONObject jsonData = new JSONObject();
         Connection conn = DbConnection.getConnection();
         String query = "SELECT * FROM product where productid =" + id;
         PreparedStatement pstmt = conn.prepareStatement(query);
 
         ResultSet rs = pstmt.executeQuery();
         while (rs.next()) {
-            int total_columns = rs.getMetaData().getColumnCount();
-            
-            for (int i = 0; i < total_columns; i++) {
-                String columnName = rs.getMetaData().getColumnLabel(i + 1).toLowerCase();
-                Object columnValue = rs.getObject(i + 1);
-                jsonData.put(columnName, columnValue);
-            }
+            jsonData = Json.createObjectBuilder()
+                            .add("productId", rs.getInt("productid"))
+                            .add("name", rs.getString("name"))
+                            .add("description", rs.getString("description"))
+                            .add("quantity", rs.getInt("quantity"))
+                            .build();
         }
-        return jsonData.toJSONString();
+        return jsonData.toString();
     }
 
     @POST
     @Path("{productid}")
     public void doPost(String str) throws SQLException, ParseException {
-        JSONObject jsonData = (JSONObject) new JSONParser().parse(str);
-        //int productid = (int) jsonData.get("id");
-        String productName = (String) jsonData.get("name");
-        String productDesc = (String) jsonData.get("description");
-        long productQty = (long) jsonData.get("quantity");
-        //System.out.println(productName + "\n" + productDesc + "\n" + productQty);
+        JSONObject jsonPostData = (JSONObject) new JSONParser().parse(str);
+        String productName = (String) jsonPostData.get("name");
+        String productDesc = (String) jsonPostData.get("description");
+        long productQty = (long) jsonPostData.get("quantity");
         doInsert("INSERT INTO product (name, description, quantity) VALUES (?, ?, ?)", productName, productDesc, productQty);
     }
 
@@ -171,11 +134,11 @@ public class ProductServlet {
     @PUT
     @Path("{productid}")
     public void doPut(@PathParam("productid") int id, String str) throws SQLException, ParseException {
-        JSONObject jsonData = (JSONObject) new JSONParser().parse(str);
-        //long productid = (long) jsonData.get("id");
-        String productName = (String) jsonData.get("name");
-        String productDesc = (String) jsonData.get("description");
-        long productQty = (long) jsonData.get("quantity");
+        JSONObject jsonPutData = (JSONObject) new JSONParser().parse(str);
+        //long productid = (long) jsonPostData.get("id");
+        String productName = (String) jsonPutData.get("name");
+        String productDesc = (String) jsonPutData.get("description");
+        long productQty = (long) jsonPutData.get("quantity");
         Connection conn = DbConnection.getConnection();
         String query = "UPDATE product SET name =\'"+ productName +"\', description =\'" + productDesc + "\', quantity =" + productQty + " WHERE productid =" + id;
         PreparedStatement pstmt = conn.prepareStatement(query);
